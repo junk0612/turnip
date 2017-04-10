9f88b150f98c83ced61e07a785a70028f1cb709a
Junichi Kobayashi <nnujabok@gmail.com>
2017-04-10 16:04:53 +0900

    Print the name of file when occured a parse error

--- lib/turnip/builder.rb
+++ lib/turnip/builder.rb
@@ -5,10 +5,15 @@ module Turnip
   class Builder
     def self.build(feature_file)
       parser = Gherkin::Parser.new
-      result = parser.parse(File.read(feature_file))
+      begin
+        result = parser.parse(File.read(feature_file))
 
-      return nil unless result[:feature]
-      Node::Feature.new(result[:feature])
+        return nil unless result[:feature]
+        Node::Feature.new(result[:feature])
+      rescue => e
+        e.message = "An error has occured in #{feature_file}.\n#{e.message}"
+        raise e
+      end
     end
   end
 end
